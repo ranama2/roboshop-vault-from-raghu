9442927da03777087ea28458da66ae1a717e1031
Raghu K <raghuk.vit@gmail.com>
2025-02-28 08:10:12 +0530

    RoboShop Code

--- module-infra/main.tf
+++ module-infra/main.tf
@@ -36,6 +36,14 @@ resource "aws_instance" "tool" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.tool.id]
 
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type             = "persistent"
+    }
+  }
+
   tags = {
     Name = var.name
   }
