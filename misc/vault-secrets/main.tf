8809fdd67406aa95e5df249d6f443e871adae471
SVR Kalluru <raghuk.vit@gmail.com>
2025-03-07 08:54:02 +0530

    Training Code

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -112,7 +112,7 @@ resource "vault_generic_secret" "roboshop-dev-shipping" {
   data_json = <<EOT
 {
 "CART_ENDPOINT" : "cart-dev.rdevopsb83.online:8080",
-"DB_HOST" : "mysql-dev.rdevopsb83.online"
+"DB_HOST" : "mysql-dev.rdevopsb83.online",
 "DB_TYPE" : "mysql",
 "APP_GIT_URL" : "https://github.com/roboshop-devops-project-v3/shipping",
 "DB_USER" : "root",
