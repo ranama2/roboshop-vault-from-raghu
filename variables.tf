7d34d81830fdf91131366ebda02cf2d34e3b29cd
Raghu K <raghuk.vit@gmail.com>
2025-03-26 07:57:36 +0530

    RoboShop Code

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "ami_id" {
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "zone_id" {
+  default = "Z01662431H5LL60AVTC0E"
+}
+
+variable "tools" {
+  default = {
+
+    vault = {
+      instance_type = "t3.small"
+      port          = 8200
+      iam_policy    = {
+        Actions     = []
+        Resource    = []
+      }
+    }
+
+    github-runner = {
+      instance_type = "t3.small"
+      port          = 443 # Dummy port
+      iam_policy    = {
+        Actions     = []
+        Resource    = []
+      }
+    }
+
+  }
+}
+
