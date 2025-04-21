14b81456412f1785424020c6175001a151f36680
Raghu K <raghuk.vit@gmail.com>
2025-04-21 08:28:41 +0530

    Training Code

--- variables.tf
+++ variables.tf
@@ -31,6 +31,19 @@ variable "tools" {
       }
     }
 
+    elk-stack = {
+      instance_type     = "i3.large"
+      ports             = {
+        elasticsearch = 9200
+        kibana        = 80
+      }
+      root_block_device = 20
+      iam_policy = {
+        Action   = ["*"]
+        Resource = []
+      }
+    }
+
   }
 }
 
